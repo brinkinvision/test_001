9c904d2c5dabfa9c2cfc39482bb50d5684c1b6c6
brinkinvision <vtikwidelearning@mail.ru>
2024-02-12 18:25:32 +0300

    add more functions

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-// import './App.css'
+import './App.css'
 import Table from './UserInformationTable'
 
 function App() {
--- src/UserInformationTable.jsx
+++ src/UserInformationTable.jsx
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from 'react';
+import './UserInformationTable.css'
 
 const UserInformationTable = () => {
   const [users, setUsers] = useState([]);
